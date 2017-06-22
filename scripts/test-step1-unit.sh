00b26146c2d88f12734f9996a53aa2620765f3fa
ricardo <ledo.tulio@gmail.com>
2017-06-22 15:32:35 -0300

    WIP

--- scripts/test-step1-unit.sh
+++ scripts/test-step1-unit.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+mkdir -p build/logs
 docker-compose exec -T magento vendor/bin/phpunit \
     --coverage-clover /opt/docker/magento/module/build/logs/clover.xml
 sudo chown -R $(whoami):$(whoami) build
